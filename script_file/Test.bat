1775fd4258b6caa3a6c37f0230bd2154a1cc7a5a
Marko Ruzic <markoruzic997@gmail.com>
2019-06-20 01:11:49 +0200

    added script_file

---
+++ script_file/Test.bat
@@ -0,0 +1,5 @@
+"model0" "speech_2ch_2.wav" "speech_2ch_2_output_model0.wav"
+
+:PCMCompare "WhiteNoise_output_model_0.wav" "WhiteNoise_output_model_1.wav" > LogFile/mo0vsMo1.txt
+
+:PCMCompare "WhiteNoise_output_model_1.wav" "WhiteNoise_output_model_2.wav" > LogFile/mo1vsMo2.txt
